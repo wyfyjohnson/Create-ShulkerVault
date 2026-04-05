a5b60af8b0693cd7c46a6b3b03391b5aa744bfd8
Wyatt <johnson.wyatt.n@gmail.com>
2026-04-05 13:21:20 -0700

    current local state

--- src/main/java/dev/wyfy/shulkervault/item/ModCreativeModeTabs.java
+++ src/main/java/dev/wyfy/shulkervault/item/ModCreativeModeTabs.java
@@ -1,6 +1,7 @@
 package dev.wyfy.shulkervault.item;
 
 import dev.wyfy.shulkervault.ShulkerVault;
+import dev.wyfy.shulkervault.block.ModBlocks;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
@@ -14,12 +15,14 @@ public class ModCreativeModeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
         DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ShulkerVault.MOD_ID);
 
-    public static final Supplier<CreativeModeTab> SHULKER_VAULT_ITEMS_TAB = CREATIVE_MODE_TAB.register("shulker_vault_items_tab",
+    public static final Supplier<CreativeModeTab> SHULKERVAULT_TAB = CREATIVE_MODE_TAB.register("shulkervault_tab",
             () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.REINFORCED_SHULKER_SHELL.get()))
-                    .title(Component.translatable("creativetab.shulkervault.shulker_vault_items"))
-                    .displayItems(itemDisplayParameters, output) -> )
-                    .build());
-
+                    .title(Component.translatable("creativetab.shulkervault.shulker_vault"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                      output.accept(ModItems.REINFORCED_SHULKER_SHELL.get());
+                      output.accept(ModBlocks.SHULKER_VAULT);
+                      output.accept(ModBlocks.ADVANCED_SHULKER_VAULT);
+                    }).build());
     public static void register(IEventBus eventBus) {
        CREATIVE_MODE_TAB.register(eventBus);
     }
